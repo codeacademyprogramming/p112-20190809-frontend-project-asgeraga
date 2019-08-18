aeeb4b3054b9f09414c560c5287900b01270e4f4
asgeraga <51189620+asgeraga@users.noreply.github.com>
2019-08-19 01:11:40 +0300

    68%

---
+++ last frontend project.min/js/main-jquery.js
@@ -0,0 +1,22 @@
+$('.slide').owlCarousel({
+    loop: true,
+    dots: true,
+    nav: false,
+    navSpeed: 800,
+    autoplay: true,
+    transitionStyle: "fade",
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        }
+    }
+
+})
+
+
